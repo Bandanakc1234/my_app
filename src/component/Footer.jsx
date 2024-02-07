ce8d59dfde01c376890ed04ba857234ddf409eb4
Aditipradhan123 <109887476+Aditipradhan123@users.noreply.github.com>
2024-02-07 14:42:50 +0545

    send

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { FaChevronRight, FaMapMarkerAlt, FaPhoneAlt, FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa'
-import { SiGmail } from "react-icons/si";
+import { FaChevronRight, FaMapMarkerAlt, FaPhoneAlt} from 'react-icons/fa'
+import { IoIosMail, IoLogoFacebook, IoLogoInstagram, IoLogoTwitter, IoLogoLinkedin } from "react-icons/io";
+
 
 
 const Footer = () => {
   return (
-    <footer >
-      <div className='p-15 bg-gray-800 text-gray-200'>
-        <div className='max-w-7xl mx-auto'>
+    <footer className='bg-blue-200 pl-16 pr-16 h' >
+      <div className='bg-gray-800 text-gray-200'>
+        <div className='max-w-7xl mx-auto p-12'>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2'>
             <div className=' p-4' >
               <h1 className='uppercase mb-3 text-xl'>Index it hub</h1>
@@ -33,7 +34,7 @@ const Footer = () => {
                 </li>
               </ul>
             </div>
-            <div className=' p-4'>
+            <div className=' p-3'>
               <h1 className='uppercase mb-3 text-xl'>Contact</h1>
               <ul className='flex flex-col'>
                 <li className='flex pb-2'>
@@ -41,38 +42,38 @@ const Footer = () => {
                   <span className='ml-1'>Kathmandu, Nepal</span>
                 </li>
                 <li className='flex pb-2'>
-                  <SiGmail size={15} className='m-1' />
+                  <IoIosMail size={17} className='m-1' />
                   <span className='ml-1'>info@indexithub.com</span>
                 </li>
                 <li className='flex pb-2'>
                   <FaPhoneAlt size={15} className='m-1' />
-                  <span className='ml-1'>9860113289</span>
+                  <span className='ml-1'>977-9860113289</span>
                 </li>
               </ul>
             </div>
-            <div className=' p-4'>
+            <div className=' p-2'>
               <h1 className='mb-3 text-xl'>Subscribe to our newsletter</h1>
               <p className='mb-2'>Monthly digest of what's new and exciting from us.</p>
               <form className='flex flex-row flex-wrap'>
-                <input type="text" placeholder='Email address' className='text-black w-2/3 text-center' />
-                <button className='p-1 ml-1 bg-blue-400 hover:bg-blue-500'>Subscribe</button>
+                <input type="text" placeholder='Email address' className='text-black w-2/3 text-center mr-1' />
+                <button className='p-1 rounded-md bg-blue-500 hover:bg-blue-700'>Subscribe</button>
               </form>
             </div>
           </div>
         </div>
-        <div className='flex flex-col border-t'>
+        <div className='flex flex-col border-t p-0'>
           <ul className='flex justify-end space-x-2 m-4'>
             <li className='hover:text-blue-500'>
-              <FaInstagram size={25} />
+              <IoLogoInstagram size={30} />
             </li>
             <li className='hover:text-blue-500'>
-              <FaFacebook size={25} />
+              <IoLogoFacebook size={30} />
             </li>
             <li className='hover:text-blue-500'>
-              <FaTwitter size={25} />
+              <IoLogoTwitter size={30} />
             </li>
             <li className='hover:text-blue-500'>
-              <FaLinkedin size={25} />
+              <IoLogoLinkedin size={30} />
             </li>
           </ul>
         </div>
