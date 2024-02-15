429416573aec44ab26c8843ad78e44c121f83be4
Aditipradhan123 <109887476+Aditipradhan123@users.noreply.github.com>
2024-02-15 16:37:45 +0545

    nice

---
+++ src/admin_dasboard/home/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import Sidenav from './components/sidenav'
+
+const Home = () => {
+  return (
+    <>
+    <Sidenav/>
+    </>
+  )
+}
+
+export default Home
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,11 +5,11 @@ import { AiOutlineMenu, AiOutlineClose, } from 'react-icons/ai'
 import { IoIosMail, IoLogoFacebook, IoLogoInstagram, IoLogoTwitter, IoLogoLinkedin } from "react-icons/io";
 
 import { useState } from 'react';
-
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+    const pathname = usePathname()
     const [menuOpen, setMenuOpen] = useState(false);
-
     const handleNav = () => {
         setMenuOpen(!menuOpen);
     }
@@ -26,22 +26,22 @@ const Navbar = () => {
                     <div className='hidden md:flex '>
                         <ul className='hidden md:flex'>
                             <Link href={'/'}>
-                                <li className='ml-10 font-bold hover:text-blue-500 text-xl'>Home</li>
+                                <li className={`ml-10 font-bold text-xl ${pathname === '/' ? 'text-blue-500' : 'hover:text-blue-500'}`}>Home</li>
                             </Link>
                             <Link href={'/about'}>
-                                <li className='ml-10 font-bold hover:text-blue-500 text-xl'>About</li>
+                                <li className={`ml-10 font-bold text-xl ${pathname === '/about' ? 'text-blue-500' : 'hover:text-blue-500'}`}>About</li>
                             </Link>
                             <Link href={'/service'}>
-                                <li className='ml-10 font-bold hover:text-blue-500 text-xl'>Service</li>
+                                <li className={`ml-10 font-bold text-xl ${pathname === '/service' ? 'text-blue-500' : 'hover:text-blue-500'}`}>Service</li>
                             </Link>
                             <Link href={'/project'}>
-                                <li className='ml-10 font-bold hover:text-blue-500 text-xl'>Project</li>
+                                <li className={`ml-10 font-bold text-xl ${pathname === '/project' ? 'text-blue-500' : 'hover:text-blue-500'}`}>Project</li>
                             </Link>
                             <Link href={'/career'}>
-                                <li className='ml-10 font-bold hover:text-blue-500 text-xl'>Career</li>
+                                <li className={`ml-10 font-bold text-xl ${pathname === '/career' ? 'text-blue-500' : 'hover:text-blue-500'}`}>Career</li>
                             </Link>
                             <Link href={'/contact'}>
-                                <li className='ml-10 font-bold hover:text-blue-500 text-xl'>Contact</li>
+                                <li className={`ml-10 font-bold text-xl ${pathname === '/contact' ? 'text-blue-500' : 'hover:text-blue-500'}`}>Contact</li>
                             </Link>
                         </ul>
                     </div>
@@ -61,22 +61,22 @@ const Navbar = () => {
                     <div className='flex-col py-4'>
                         <ul>
                             <Link href={'/'}>
-                                <li className='py-4 cursor-pointer font-bold hover:text-white'>Home</li>
+                                <li className={`py-4 cursor-pointer font-bold ${pathname === '/' ? 'text-white' : 'hover:text-white'}`}>Home</li>
                             </Link>
                             <Link href={'/about'}>
-                                <li className='py-4 cursor-pointer font-bold hover:text-white'>About</li>
+                                <li className={`py-4 cursor-pointer font-bold ${pathname === '/about' ? 'text-white' : 'hover:text-white'}`}>About</li>
                             </Link>
                             <Link href={'/service'}>
-                                <li className='py-4 cursor-pointer font-bold hover:text-white'>Services</li>
+                                <li className={`py-4 cursor-pointer font-bold ${pathname === '/service' ? 'text-white' : 'hover:text-white'}`}>Services</li>
                             </Link>
                             <Link href={'/project'}>
-                                <li className='py-4 cursor-pointer font-bold hover:text-white'>Project</li>
+                                <li className={`py-4 cursor-pointer font-bold ${pathname === '/project' ? 'text-white' : 'hover:text-white'}`}>Project</li>
                             </Link>
                             <Link href={'/career'}>
-                                <li className='py-4 cursor-pointer font-bold hover:text-white'>Career</li>
+                                <li className={`py-4 cursor-pointer font-bold ${pathname === '/career' ? 'text-white' : 'hover:text-white'}`}>Career</li>
                             </Link>
                             <Link href={'/contact'}>
-                                <li className='py-4 cursor-pointer font-bold hover:text-white'>Contact</li>
+                                <li className={`py-4 cursor-pointer font-bold ${pathname === '/contact' ? 'text-white' : 'hover:text-white'}`}>Contact</li>
                             </Link>
                         </ul>
                     </div>
