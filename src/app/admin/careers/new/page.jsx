e63b0d767bf2ff99c3e9a402fc0f3b530ae512e6
Bandanakc1234 <109887497+Bandanakc1234@users.noreply.github.com>
2024-03-11 13:06:21 +0545

    update

--- src/app/admin/careers/new/page.jsx
+++ src/app/admin/careers/new/page.jsx
@@ -21,19 +21,19 @@ const Career = () => {
                 }
             })
     }
-
     console.log(career)
+
     return (
-        <div className='h-screen w-full bg-red-50'>
-            <div className='border-2 shadow-lg bg-white rounded-md xl:mt-36 md:mt-28 mt-20 xl:ml-80 lg:ml-56 md:ml-32 ml-20 lg:w-2/5 xl:p-10 md:p-8 p-3 h-max'>
-                <h1 className='font-bold pb-4 lg:text-3xl lg:text-left text-center md:text-2xl'>Add Career</h1>
-                <table className='lg:w-full w-11/12 md:text-xl text-sm'> 
+        <div className='flex justify-center h-screen bg-blue-200 w-screen'>
+            <div className='border-2 shadow-lg bg-white rounded-md xl:mt-16 md:mt-12 mt-5 xl:ml-80 lg:ml-56 md:ml-28 ml-20 lg:w-2/5 md:w-4/6 w-4/6 xl:p-10 md:p-6 p-2 h-max'>
+                <h1 className='font-bold pb- lg:text-3xl lg:text-left text-center md:text-2xl'>Add Career</h1>
+                <table className='lg:w-full w-10/12 md:text-xl text-sm'> 
                     <tr className=''>
                         <td className=''>
                             <label className=' md:text-lg text-sm'>Career_Title:</label>
                         </td>
                         <td className=''>
-                            <input type="text" name="title" className='border-2 border-black rounded-md md:h-10 h-8 my-3 w-full' onChange={setCareer} />
+                            <input type="text" name="title" className='border-2 border-black rounded-md md:h-8 h-7 w-full' onChange={setCareer} />
                         </td>
                     </tr>
                     <tr className=''>
@@ -41,7 +41,7 @@ const Career = () => {
                             <label className='md:text-lg text-sm'>vacancyNumber:</label>
                         </td>
                         <td className=''>
-                            <input type="text" name="vacancyNumber" className='border-2 border-black rounded-md md:h-10 h-8 my-3 w-full' onChange={setCareer} />
+                            <input type="text" name="vacancyNumber" className='border-2 border-black rounded-md md:h-8 h-7 w-full' onChange={setCareer} />
                         </td>
                     </tr>
                     <tr>
@@ -49,7 +49,7 @@ const Career = () => {
                             <label className='md:text-lg'>offered_salary:</label>
                         </td>
                         <td>
-                            <input type="text" name="offered_salary" className='border-2 border-black rounded-md md:h-10 h-8 my-3 w-full' onChange={setCareer} />
+                            <input type="text" name="offered_salary" className='border-2 border-black rounded-md md:h-8 h-7 w-full' onChange={setCareer} />
 
                         </td>
                     </tr>
@@ -58,7 +58,7 @@ const Career = () => {
                             <label className='md:text-lg'>posted_date:</label>
                         </td>
                         <td>
-                            <input type="text" name="posted_date" className='border-2 border-black rounded-md md:h-10 h-8 my-3 w-full' onChange={setCareer} />
+                            <input type="text" name="posted_date" className='border-2 border-black rounded-md md:h-8 h-7 w-full' onChange={setCareer} />
                         </td>
                     </tr>
                     <tr>
@@ -66,11 +66,11 @@ const Career = () => {
                             <label className='md:text-lg'>deadline:</label>
                         </td>
                         <td>
-                            <input type="text" name="deadline" className='border-2 border-black rounded-md md:h-10 h-8 my-3 w-full' onChange={setCareer} />
+                            <input type="text" name="deadline" className='border-2 border-black rounded-md md:h-8 h-7 w-full' onChange={setCareer} />
                         </td>
                     </tr>
                 </table>
-                <button onClick={handleSubmit} className="border border-none bg-blue-600 rounded-md md:w-20 md:h-10 w-14 h-7 lg:mt-8 md:text-lg text-sm"><a href="#" className=' hover:text-white'>Add</a></button>
+                <button onClick={handleSubmit} className="border border-none bg-blue-600 rounded-md md:w-20 md:h-10 w-14 h-7 lg:mt-8 mt-3 md:text-lg text-sm"><a href="#" className=' hover:text-white'>Add</a></button>
             </div>
         </div>
     )
--- src/app/admin/careers/page.jsx
+++ src/app/admin/careers/page.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+'use client';
+import { view_career } from '@/api/careerAPI';
+import React, { useEffect, useState } from 'react'
 
 const Careers = () => {
+  let [careers, setCareers] = useState([])
+
+  useEffect(() => {
+    view_career()
+      .then(data => {
+        if (data.error) {
+          console.log(data.error)
+        }
+        else {
+          console.log(data)
+          setCareers(data)
+        }
+      })
+  }, [])
+
   return (
-    <div className='text-black flex flex-col justify-center'>this is all Careers page
+    <div className='mt-28 ms-28 bg-slate-300 w-full m-auto'>
+      <h1>Careers</h1>
+      <div className='text-black flex flex-col'>
+        {
+          careers.map(career => {
+            return <div key={career._id} className='p-5 border border-3 border-blue-700 m-5 '>
+              <h1>Title: {career.career_title}</h1>
+              <h1>No. of Positions: {career.vacancyNumber}</h1>
+              <h1>Offered Salary: {career.career_title}</h1>
+              <h1>Job Description: {career.career_title}</h1>
+              <h1>Required Qualifications: {career.career_title}</h1>
+              <h1>Posted Date: {career.career_title}</h1>
+              <h1>Application Deadline: {career.career_title}</h1>
+              <h1>Status: </h1>
+              <button> Edit </button>
+              <button> Remove </button>
+            </div>
+          })
+        }
+      </div>
+
     </div>
   )
 }
