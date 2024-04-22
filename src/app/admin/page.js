14f5455612416202566a557c2e347c2df7831582
kaveripunu <kaveripunu@gmail.com>
2024-04-22 16:14:52 +0545

    Modification

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -71,64 +71,63 @@ export default function Home() {
   return (
     <>
       {/* Recent Job Posts */}
-      <div className="flex justify-between w-11/12 mx-auto pb-8">
-        <div className="recentjobs w-3/4 ">
-          <h1 className="text-2xl font-bold py-1">Recent Job Posts</h1>
+      <div className="flex flex-col lg:flex-row justify-between w-11/12 mx-auto pb-8 px-5 md:px-0">
+        <div className="recentjobs w-full lg:w-3/4 order-2 lg:order-1">
+          <h1 className="text-xl md:text-2xl font-bold py-1">Recent Job Posts</h1>
           {careers?.length > 0 &&
             careers.slice(1, limit).map((career) => {
               return (
                 <div
                   key={career._id}
-                  className="w-4/5 p-5 border-2 bg-white rounded-md shadow-lg my-2 "
+                  className="w-full lg:w-4/5 p-5 border-2 bg-white rounded-md shadow-lg my-2 "
                 >
-                  <h1 className="text-2xl font-semibold capitalize py-1">
+                  <h1 className="text-lg md:text-xl font-semibold capitalize py-1">
                     {career.career_title}
                   </h1>
-                  <h2 className="text-lg font-medium py-1">
+                  <h2 className="text-sm md:text-base font-medium py-1">
                     No. Of Positions:
-                    <span className="text-base capitalize">
+                    <span className="text-xs md:text-sm capitalize">
                       {" "}
                       {career.vacancyNumber}
                     </span>
                   </h2>
-                  <h2 className="text-lg font-medium py-1">
+                  <h2 className="text-sm md:text-base font-medium py-1">
                     Posted Date:
-                    <span className="text-base capitalize">
+                    <span className="text-xs md:text-sm capitalize">
                       {" "}
                       {career.posted_date}
                     </span>
                   </h2>
-                  <h2 className="text-lg font-medium py-1">
+                  <h2 className="text-sm md:text-base font-medium py-1">
                     Application Deadline:
-                    <span className="text-base capitalize">
+                    <span className="text-xs md:text-sm capitalize">
                       {" "}
                       {career.deadline}
                     </span>
                   </h2>
-                  {/* <h2 className="text-lg font-medium">Status: </h2> */}
                 </div>
               );
             })}
           <Link href={`/admin/careers`} className="py-2">
-            <button className="bg-blue-500 h-9 text-lg rounded-md cursor-pointer w-28 hover:bg-blue-700 text-white">
+            <button className="bg-blue-500 h-9 text-sm md:text-lg rounded-md cursor-pointer w-28 hover:bg-blue-700 text-white">
               Read More
             </button>
           </Link>
         </div>
-        <div className="profile w-1/4 bg-gray-50">
-          <div className="profile-pic flex flex-col justify-center items-center h-80 w-80 font-bold">
+        <div className="profile w-full lg:w-1/4 order-1 lg:order-2">
+          <div className="profile-pic flex flex-col justify-center items-center w-full font-bold">
             <h1>
               <img
                 src={`${API}/${userImage}`}
                 alt=""
-                className="h-64 border mt-12 rounded-md"
+                className="md:h-36 h-24 md:w-36 w-24 border mt-12 rounded-md"
               />
             </h1>
-            <h1 className="capitalize mt-2">
+            <h1 className="text-sm md:text-base capitalize mt-2 ">
               {userfirstname} {userlastname}
             </h1>
-            <h1 className="capitalize">{userposition}</h1>
-            <button className="bg-blue-600 rounded-md p-2 mt-2">
+            <h1 className="text-sm md:text-base capitalize">{userposition}</h1>
+            <button className="bg-blue-600 rounded-md p-2 mt-2 text-sm md:text-base mb-3 lg:mb-0">
               Edit Profile
             </button>
           </div>
@@ -136,38 +135,45 @@ export default function Home() {
       </div>
 
       {/* Projects */}
-      <div className="projects w-11/12 mx-auto pb-8">
-        <h1 className="text-2xl font-bold py-1">Projects</h1>
+      <div className="projects w-11/12 mx-auto pb-8 px-5 md:px-0">
+        <h1 className="text-xl md:text-2xl font-bold py-1">Projects</h1>
         <div className="w-full flex flex-col items-center">
           {projects?.length > 0 &&
             projects.slice(1, limit).map((project) => {
               return (
                 <div
                   key={project._id}
-                  className="p-5 bg-white border-2 rounded-md shadow-lg my-2 flex w-full items-center "
+                  className="sm:px-5 py-5 bg-white border-2 rounded-md shadow-lg my-2 flex flex-col sm:flex-row w-full items-center "
                 >
-                  <h1 className="w-1/3">
+                  <h1 className="w-4/5 md:w-1/3 flex justify-center">
                     <img
                       src={`${API}/${project.project_image}`}
                       alt={project.project_image}
-                      className="rounded-md w-full"
-                      style={{ height: "300px" }}
+                      className="rounded-md w-full dashboard-projecimg"
+                      style={{
+                        // "@media (min-width: 768px)": {
+                        //   height: "300px", 
+                        //   // width: "100%"
+                        // },
+                        height: "265px",
+                        // width: "250px",
+                      }}
                     />
                   </h1>
-                  <div className="w-3/5 flex flex-col justify-center px-9">
-                    <h1 className="text-2xl font-semibold capitalize py-1">
+                  <div className="w-4/5 md:w-3/5 flex flex-col justify-center items-center sm:items-start sm:px-9">
+                    <h1 className="text-lg lg:text-xl font-semibold capitalize py-1">
                       {project.project_title}
                     </h1>
-                    <h2 className="text-lg font-medium py-1">
+                    <h2 className="text-sm lg:text-base font-medium py-1">
                       Category:
-                      <span className="text-base capitalize">
+                      <span className="text-xs md:text-sm capitalize">
                         {" "}
                         {project.category}{" "}
                       </span>{" "}
                     </h2>
-                    <h2 className="text-lg font-medium py-1">
+                    <h2 className="text-sm lg:text-base font-medium py-1">
                       Language:
-                      <span className="text-base font-normal capitalize">
+                      <span className="text-xs md:text-sm font-normal capitalize">
                         {" "}
                         {project.language}{" "}
                       </span>
@@ -177,7 +183,7 @@ export default function Home() {
               );
             })}
           <Link href={`/admin/projects`} className="py-2">
-            <button className="bg-blue-500 h-9 text-lg rounded-md cursor-pointer w-28 hover:bg-blue-700 text-white">
+            <button className="bg-blue-500 h-9 text-sm md:text-lg rounded-md cursor-pointer w-28 hover:bg-blue-700 text-white">
               Read More
             </button>
           </Link>
@@ -186,133 +192,45 @@ export default function Home() {
       
 
       {/* Applicants */}
-      <div className="applications w-11/12 mx-auto pb-8">
-        <h1 className="text-2xl font-bold py-1">Applicants</h1>
-        <div className="w-full p-5 border-2 bg-white rounded-md shadow-lg my-2">
-          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="applications w-11/12 mx-auto pb-8 px-5 md:px-0">
+        <h1 className="text-xl md:text-2xl font-bold py-1">Applicants</h1>
+        <div className="w-full shadow-lg my-2">
+          <div class="relative overflow-x-auto shadow-md rounded-lg overflow-hidden">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+              <thead class="text-xs md:text-sm text-white uppercase bg-blue-500">
                 <tr>
-                  <th scope="col" class="px-6 py-3">
-                    Product name
+                  <th scope="col" class="px-6 md:px-2 py-3 md:py-1">
+                    Applicant Name
                   </th>
-                  <th scope="col" class="px-6 py-3">
-                    Color
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Category
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Price
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Edit</span>
+                  <th scope="col" class="px-6 md:px-2 py-3 md:py-1">
+                    Career Applied
                   </th>
                 </tr>
               </thead>
-              <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Apple MacBook Pro 17"
-                  </th>
-                  <td class="px-6 py-4">Silver</td>
-                  <td class="px-6 py-4">Laptop</td>
-                  <td class="px-6 py-4">$2999</td>
-                  <td class="px-6 py-4 text-right">
-                    <a
-                      href="#"
-                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              <tbody className="text-xs md:text-sm overflow-y-auto max-h-[400px]">
+                {applicants?.length > 0 &&
+                  applicants.map((applicant) => {
+                    const appcareer = careers.find((career) => career._id === applicant.career)
+                    const career_title = appcareer ? appcareer.career_title : 'Unkown Career'
+                    return (<tr
+                      key={applicant._id}
+                      className="bg-white border-b hover:bg-gray-50"
                     >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Microsoft Surface Pro
-                  </th>
-                  <td class="px-6 py-4">White</td>
-                  <td class="px-6 py-4">Laptop PC</td>
-                  <td class="px-6 py-4">$1999</td>
-                  <td class="px-6 py-4 text-right">
-                    <a
-                      href="#"
-                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Magic Mouse 2
-                  </th>
-                  <td class="px-6 py-4">Black</td>
-                  <td class="px-6 py-4">Accessories</td>
-                  <td class="px-6 py-4">$99</td>
-                  <td class="px-6 py-4 text-right">
-                    <a
-                      href="#"
-                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
+                      <Link href={`/admin/careers/appliedCareer`} className="py-2">
+
+                        <td className="px-6 md:px-2 py-3 md:py-1 font-medium text-gray-900 whitespace-nowrap capitalize">
+                          {applicant.first_name} {applicant.last_name}
+                        </td>
+                      </Link>
+                      <td className="px-6 md:px-2 py-3 md:py-1 capitalize">{career_title}</td>
+                    </tr>
+                    );
+                  })}        
               </tbody>
             </table>
           </div>
-
-          {applicants?.length > 0 &&
-            applicants.map((applicant) => {
-              return (
-                <div key={applicant._id} className="">
-                  <table className="border-4">
-                    <thead className="border-4">
-                      <tr className="border-4">
-                        <th>Song</th>
-                        <th>Artist</th>
-                        <th>Year</th>
-                      </tr>
-                    </thead>
-                    <tbody className="border-4">
-                      <tr className="border-4">
-                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
-                      </tr>
-                      <tr className="border-4">
-                        <td>Witchy Woman</td>
-                        <td>The Eagles</td>
-                        <td>1972</td>
-                      </tr>
-                      <tr className="border-4">
-                        <td>Shining Star</td>
-                        <td>Earth, Wind, and Fire</td>
-                        <td>1975</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              );
-            })}
         </div>
       </div>
     </>
   );
-}
-
-{
-  /* <Link href="/admin/careers/appliedCareer">
-                  <li> Name: {applicant.first_name} {applicant.last_name} </li>
-                </Link> */
 }
